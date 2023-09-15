19a7493725d595cc9e3d568122021f672899b94f
Bruno Correa Leite <bcorrea@pop-os.localdomain>
2023-09-15 05:00:06 -0300

    feat: added error handling for components fetching data from an api

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,17 +1,26 @@
 import axios from "axios";
 
-import PostTitle from "./postTitle";
+import PostTitle from "./components/postTitle";
 
 export default async function Home() {
-  const posts = await axios.get("https://jsonplaceholder.typicode.com/posts");
-
-  return (
-    <div className="flex flex-col items-center mt-5">
-      {posts.data.map((post) => (
-        <div className="block mb-5 bg-gray-900 w-10/12 px-4 py-2 rounded-lg">
-          <PostTitle post={post} />
-        </div>
-      ))}
-    </div>
-  );
+  try {
+    const postsResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+    const posts = postsResponse.data;
+    return (
+      <div className="flex flex-col items-center mt-5">
+        {posts.map((post) => (
+          <div className="block mb-5 bg-gray-900 w-10/12 px-4 py-2 rounded-lg">
+            <PostTitle post={post} />
+          </div>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.log(error);
+    return (
+      <h1 className="text-4xl font-bold mt-5">Failed to load main page!</h1>
+    );
+  }
 }
--- client/src/app/threads/[id]/page.js
+++ client/src/app/threads/[id]/page.js
@@ -1,31 +1,36 @@
 import axios from "axios";
 
 export default async function Thread({ params }) {
-  const threadRequest = await axios.get(
-    `http://localhost:8080/api/threads/${params.id}`,
-  );
-  const threadData = threadRequest.data;
-  return (
-    <div className="flex flex-col w-10/12 mt-5 items-center">
-      <div className="mb-5 bg-gray-900 rounded-lg px-4 py-2">
-        <h1 className="text-3xl font-bold mb-2">{threadData.post.title}</h1>
-        <p className="text-gray-300">{threadData.post.body}</p>
-      </div>
-      <div className="bg-gray-900 px-4 py-2 w-[98%]">
-        {threadData.comments.map((comment) => (
-          <div className="mt-6 px-2 border-l border-gray-500/50">
-            <div className="flex items-baseline">
-              <p className="text-cyan-400 text-sm bg-cyan-950/30 rounded px-2">
-                {comment.name}
-              </p>
-              <p className="ml-2 text-gray-500/75 text-xs">
-                email: {comment.email}
-              </p>
+  try {
+    const threadRequest = await axios.get(
+      `http://localhost:8080/api/threads/${params.id}`,
+    );
+    const thread = threadRequest.data;
+    return (
+      <div className="flex flex-col w-10/12 mt-5 items-center">
+        <div className="mb-5 bg-gray-900 rounded-lg px-4 py-2">
+          <h1 className="text-3xl font-bold mb-2">{thread.post.title}</h1>
+          <p className="text-gray-300">{thread.post.body}</p>
+        </div>
+        <div className="bg-gray-900 px-4 py-2 w-[98%]">
+          {thread.comments.map((comment) => (
+            <div className="mt-6 px-2 border-l border-gray-500/50">
+              <div className="flex items-baseline">
+                <p className="text-cyan-400 text-sm bg-cyan-950/30 rounded px-2">
+                  {comment.name}
+                </p>
+                <p className="ml-2 text-gray-500/75 text-xs">
+                  email: {comment.email}
+                </p>
+              </div>
+              <p className="text-base text-gray-200">{comment.body}</p>
             </div>
-            <p className="text-base text-gray-200">{comment.body}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.log(error);
+    return <h1 className="text-4xl font-bold mt-5">Post not found!</h1>;
+  }
 }
